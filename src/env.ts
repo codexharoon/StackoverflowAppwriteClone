8fdcc54efbcd58a115ca12f4bcc469271a7c725b
Muhammad Haroon <theharoonsocial@gmail.com>
2024-07-31 15:38:14 +0500

    created folder structure

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+const env = {
+  appwrite: {
+    endpoint: process.env.APPWRITE_ENDPOINT,
+    projectId: process.env.APPWRITE_PROJECT_ID,
+    apikey: process.env.APPWRITE_API_KEY,
+  },
+};
+
+export default env;
